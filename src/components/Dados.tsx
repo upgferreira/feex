030d4126369359bca35abbd382b1b9586a26859e
upgferreira <upgferreira@gmail.com>
2026-06-14 09:43:31 -0300

    Update Dados.tsx

--- src/components/Dados.tsx
+++ src/components/Dados.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef } from 'react';
+import React, { useState, useMemo, useRef, useEffect } from 'react';
 import {
   ResponsiveContainer, PieChart, Pie, Cell, Tooltip,
   BarChart, CartesianGrid, XAxis, YAxis, Bar,
